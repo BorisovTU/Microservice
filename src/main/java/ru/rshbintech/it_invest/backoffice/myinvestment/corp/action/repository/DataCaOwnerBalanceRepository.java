ace134e3b150ef560b07580afdc4c103a2828c30
BorisovTU <borisovtimofey@gmail.com>
2025-12-11 12:44:20 +0300

    Delete src directory

--- src/main/java/ru/rshbintech/it_invest/backoffice/myinvestment/corp/action/repository/DataCaOwnerBalanceRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.rshbintech.it_invest.backoffice.myinvestment.corp.action.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.rshbintech.it_invest.backoffice.myinvestment.corp.action.entity.DataCaOwnerBalance;
-
-@Repository
-public interface DataCaOwnerBalanceRepository extends JpaRepository<DataCaOwnerBalance, Long> {
-}
