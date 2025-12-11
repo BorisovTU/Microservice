5c56b102344aaa18910930935dc5babec857294e
BorisovTU <borisovtimofey@gmail.com>
2025-12-11 12:42:25 +0300

    Delete example01 directory

--- example01/src/main/java/ru/rshbintech/it_invest/backoffice/myinvestment/fx_client_trades_service_sofr/dao/TradeDao.java
+++
@@ -1,9 +0,0 @@
-package ru.rshbintech.it_invest.backoffice.myinvestment.fx_client_trades_service_sofr.dao;
-
-import ru.rshbintech.it_invest.backoffice.myinvestment.fx_client_trades_service_sofr.dto.TradeDto;
-
-public interface TradeDao {
-
-    void saveTrade(TradeDto tradeDto);
-
-}
